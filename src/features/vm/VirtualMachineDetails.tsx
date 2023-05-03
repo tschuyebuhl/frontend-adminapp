2f71c41c240cfee6e086648a90d7d5b67ee8f6ad
pyoxa <ksawery.wrobel@tecna.pl>
2023-05-03 21:41:12 +0200

    working on vm details

--- src/features/vm/VirtualMachineDetails.tsx
+++ src/features/vm/VirtualMachineDetails.tsx
@@ -1,11 +1,17 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { getVirtualMachine } from './VirtualMachine';
 import { useQuery } from '@tanstack/react-query';
+import React from 'react';
 
 import {
   Box,
   Button,
   CircularProgress,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
   Typography,
 } from '@mui/material';
 
@@ -14,6 +20,9 @@ export function VirtualMachineDetails() {
   let navigate = useNavigate();
   const results = useQuery(['name', name], getVirtualMachine);
   const virtualMachine = results.data;
+
+  const [open, setOpen] = React.useState(false);
+
   if (results.isLoading) {
     return (
       <Box
@@ -28,10 +37,26 @@ export function VirtualMachineDetails() {
       </Box>
     );
   }
-  
+
   function handleClick() {
     navigate('/virtual-machines');
   }
+
+  function handleDelete() {
+    fetch(`/api/v1/virtual-machines/${name}`, { method: 'DELETE' })
+      .then(() => navigate('/virtual-machines'))
+      .catch((error) => console.error(error));
+    handleClose();
+  }
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
     <Box sx={{ p: 2 }}>
       <Typography variant="h4" sx={{ mb: 2 }}>
@@ -39,19 +64,41 @@ export function VirtualMachineDetails() {
       </Typography>
       <Button onClick={handleClick}>Go back</Button> <br />
       <Box sx={{ mb: 2 }}>
-  <Typography variant="h6">Name: {virtualMachine?.Name}</Typography>
-</Box>
-<Box sx={{ mb: 2 }}>
-  <Typography variant="h6">
-    RAM: {virtualMachine?.MemoryMB ? virtualMachine.MemoryMB / 1024 : null} GB
-  </Typography>
-</Box>
-<Box sx={{ mb: 2 }}>
-  <Typography variant="h6">vCPU: {virtualMachine?.NumCpus}</Typography>
-</Box>
-<Button aria-label="Edit Virtual Machine">Edit VM</Button>
-<Button aria-label="Run Playbook">Run Playbook</Button>
-<Button aria-label="Clone Virtual Machine">Clone VM</Button>
+        <Typography variant="h6">Name: {virtualMachine?.Name}</Typography>
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="h6">
+          RAM: {virtualMachine?.MemoryMB ? virtualMachine.MemoryMB / 1024 : null} GB
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <Typography variant="h6">vCPU: {virtualMachine?.NumCpus}</Typography>
+      </Box>
+      <Button aria-label="Edit Virtual Machine">Edit VM</Button>
+      <Button aria-label="Run Playbook">Run Playbook</Button>
+      <Button aria-label="Clone Virtual Machine">Clone VM</Button>
+      <Button
+        color="warning"
+        variant="contained"
+        aria-label="Delete Virtual Machine"
+        onClick={handleClickOpen}
+      >
+        Delete VM
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Delete Virtual Machine</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this virtual machine?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleDelete} color="warning" variant="contained">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
--- src/theme.tsx
+++ src/theme.tsx
@@ -14,6 +14,12 @@ const theme = createTheme({
       dark: '#0b84ba', // darker shade of the color
       contrastText: '#fff', // text color to use on top of the color
     },
+    warning: {
+      main: '#DC0404', // your chosen color
+      light: '#E33A3A', // lighter tint of the color
+      dark: '#A60000', // darker shade of the color
+      contrastText: '#fff', // text color to use on top of the color
+    },
   },
 });
 
